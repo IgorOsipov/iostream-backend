bfad784e51ce0be83c37f440894dfad50ff3fd8b
IgorOsipov <awesomeigor@gmail.com>
2025-04-09 09:58:06 +0300

    Update dependencies in bun.lock and package.json. Upgrade @apollo/server to version 4.12.0 and @nestjs/common to version 11.0.13. Update Prisma Client to version 6.6.0, reflecting changes in generated files and ensuring compatibility with the latest features.

--- prisma/generated/index.d.ts
+++ prisma/generated/index.d.ts
@@ -244,8 +244,8 @@ export namespace Prisma {
   export import Exact = $Public.Exact
 
   /**
-   * Prisma Client JS version: 6.5.0
-   * Query Engine version: 173f8d54f8d52e692c7e27e72a88314ec7aeff60
+   * Prisma Client JS version: 6.6.0
+   * Query Engine version: f676762280b54cd07c770017ed3711ddde35f37a
    */
   export type PrismaVersion = {
     client: string
@@ -1761,7 +1761,7 @@ export namespace Prisma {
 
   /**
    * Fields of the User model
-   */ 
+   */
   interface UserFieldRefs {
     readonly id: FieldRef<"User", 'String'>
     readonly email: FieldRef<"User", 'String'>
@@ -2869,7 +2869,7 @@ export namespace Prisma {
 
   /**
    * Fields of the Token model
-   */ 
+   */
   interface TokenFieldRefs {
     readonly id: FieldRef<"Token", 'String'>
     readonly token: FieldRef<"Token", 'String'>
@@ -3384,7 +3384,7 @@ export namespace Prisma {
 
 
   /**
-   * Field references 
+   * Field references
    */
 
 
